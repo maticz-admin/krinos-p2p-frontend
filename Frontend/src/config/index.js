7a2e1e4cd1ce0b001eb9b39a11bc96c99d67d38f
Karthickraja-maticz <muthuskmaticz@gmail.com>
2025-08-20 09:59:23 +0530

    configPage_Updated

--- Frontend/src/config/index.js
+++ Frontend/src/config/index.js
@@ -1,23 +1,23 @@
 let key = {};
-let env='maticzdemo' //production or local
+let env = '' //production or local
 if (env === "demo") {
-    const API_URL = "https://backp2p-stage.krinos.app"    
+    const API_URL = "https://backp2p-stage.krinos.app"
     //const API_URL = 'https://krinosp2p-backend.maticz.in';
     key = {
-        fronturl : "https://uat-p2p.krinos.app", 
+        fronturl: "https://uat-p2p.krinos.app",
         //fronturl : "https://krinosp2p.maticz.in",
         secretOrKey: "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3",
         CRYPTO_SECRET_KEY: "1234567812345678",
         RECAPTCHA_SITE_KEY: "6LcK8vEqAAAAAD06JyU22cbe42oqCgZRhjkDbP8L", //local
-        API_URL: "https://backp2p-stage.krinos.app", 
+        API_URL: "https://backp2p-stage.krinos.app",
         // API_URL: 'https://krinosp2p-backend.maticz.in',
-        FRONT_URL: "https://uat-p2p.krinos.app" , 
+        FRONT_URL: "https://uat-p2p.krinos.app",
         //FRONT_URL: "https://krinosp2p.maticz.in",
         ADMIN_URL: 'https://uatadmin-p2p.krinos.app',
-        SOCKET_URL: "https://backp2p-stage.krinos.app", 
+        SOCKET_URL: "https://backp2p-stage.krinos.app",
         //SOCKET_URL:'https://krinosp2p-backend.maticz.in',
         getGeoInfo: "https://geolocation-db.com/json/",
-        AUTHENTICATOR_URL : {
+        AUTHENTICATOR_URL: {
             PLAY_STORE: "https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2",
             APP_STORE: "https://apps.apple.com/us/app/google-authenticator/id388497605",
         },
@@ -28,7 +28,7 @@ else if (env === "maticzdemo") {
     const API_URL = 'https://krinosp2p-backend.maticz.in/';
     key = {
         // fronturl : "https://uat-p2p.krinos.app", 
-        fronturl : "https://krinosp2p.maticz.in",
+        fronturl: "https://krinosp2p.maticz.in",
         secretOrKey: "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3",
         CRYPTO_SECRET_KEY: "1234567812345678",
         RECAPTCHA_SITE_KEY: "6LcNAH4rAAAAAMcji4ul5LhrLqALIWjglOhm-aEx", //local
@@ -38,9 +38,9 @@ else if (env === "maticzdemo") {
         FRONT_URL: "https://krinosp2p.maticz.in",
         ADMIN_URL: 'https://uatadmin-p2p.krinos.app/',
         // SOCKET_URL: "https://backp2p-stage.krinos.app/", 
-        SOCKET_URL:'https://krinosp2p-backend.maticz.in/',
+        SOCKET_URL: 'https://krinosp2p-backend.maticz.in/',
         getGeoInfo: "https://geolocation-db.com/json/",
-        AUTHENTICATOR_URL : {
+        AUTHENTICATOR_URL: {
             PLAY_STORE: "https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2",
             APP_STORE: "https://apps.apple.com/us/app/google-authenticator/id388497605",
         },
@@ -49,7 +49,7 @@ else if (env === "maticzdemo") {
 else {
     const API_URL = 'http://localhost'
     key = {
-        fronturl : "https://krinosp2p.maticz.in",
+        fronturl: "https://krinosp2p.maticz.in",
         secretOrKey: "vOVH6sdmpNWjRRIqCc7rdxs01lwHzfr3",
         CRYPTO_SECRET_KEY: "1234567812345678",
         RECAPTCHA_SITE_KEY: "6LfUI_IqAAAAAI_l63qv0vh08os6U1qMmQRzgitA",    //"6Lfa3NYqAAAAAOPNURwGG_sO4YqgDX5iwJZmj7T1", 
@@ -57,7 +57,7 @@ else {
         FRONT_URL: 'http://localhost:3000', //'http://localhost:3000',
         ADMIN_URL: 'http://localhost:3001/admin',
         SOCKET_URL: `${API_URL}:2054`,
-        getGeoInfo:  "https://geolocation-db.com/json/",
+        getGeoInfo: "https://geolocation-db.com/json/",
         AUTHENTICATOR_URL: {
             PLAY_STORE: "https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2",
             APP_STORE: "https://apps.apple.com/us/app/google-authenticator/id388497605",
@@ -68,7 +68,7 @@ else {
 
 export default {
     ...key,
-    ...{ SITE_DETAIL : require('./siteConfig').default },
+    ...{ SITE_DETAIL: require('./siteConfig').default },
     NumOnly: (/[^0-9]/g),
-    adminViewUrl: [ ["/trade/","?from=admin"] ],
+    adminViewUrl: [["/trade/", "?from=admin"]],
 };
